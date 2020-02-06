3623f9d84d659d9da8b6a32283e916c5c575c663
yuanxindong <yuanxin.me@gmail.com>
2020-02-06 22:59:30 +0800

    add: float2bit

--- helper.js
+++ helper.js
@@ -1,10 +1,20 @@
 /**
  * 返回[start, end]内的随机整数
  * 
- * @param {start}
- * @param {end}
+ * @param {number} start
+ * @param {number} end
  * @return {number}
  */
 module.exports.genRandom = function (start, end) {
     return Math.floor(Math.random() * (end - start + 1) + start)
+}
+
+/**
+ * 将浮点数转化为2进制浮点数
+ * 
+ * @param {number} num
+ * @return {number}
+ */
+module.exports.float2bit = function (num) {
+    return  Math.floor(num * 100) / 100
 }
